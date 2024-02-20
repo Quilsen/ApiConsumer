e628642f15b036e95a047080895a5badf83a73ab
Quilsen <70715580+Quilsen@users.noreply.github.com>
2024-02-20 11:24:48 +0100

    added exception handling for restclient

--- src/main/java/com/apiconsumer/apiconsumer/infractructure/GithubApiClientExceptionHandler.java
+++ src/main/java/com/apiconsumer/apiconsumer/infractructure/GithubApiClientExceptionHandler.java
@@ -6,11 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.xml.sax.ErrorHandler;
 
 @ControllerAdvice
 public class GithubApiClientExceptionHandler {
     @ExceptionHandler(FeignException.class)
     public ResponseEntity<CustomResponse> handleFeignStatusException(FeignException e){
         return new ResponseEntity<>(new CustomResponse(e.status(), e.getMessage()),HttpStatus.resolve(e.status()));
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<CustomResponse> handleRestClientStatusException(HttpClientErrorException e){
+        return new ResponseEntity<>(new CustomResponse(e.getStatusCode().value(), e.getMessage()), HttpStatus.resolve(e.getStatusCode().value()));
     }
 }
