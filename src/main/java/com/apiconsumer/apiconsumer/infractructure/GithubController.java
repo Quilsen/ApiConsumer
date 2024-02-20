07b5e954252ff8bf3c50586223244e3e3e181f95
Quilsen <70715580+Quilsen@users.noreply.github.com>
2024-02-20 09:07:06 +0100

    adding virtual threads

--- src/main/java/com/apiconsumer/apiconsumer/infractructure/GithubController.java
+++ src/main/java/com/apiconsumer/apiconsumer/infractructure/GithubController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @AllArgsConstructor
 @RestController
@@ -20,7 +21,7 @@ class GithubController {
     private final GithubService githubService;
 
     @GetMapping("/{username}")
-    ResponseEntity<List<Response>> listUserRepositories(@PathVariable String username) {
+    ResponseEntity<List<Response>> listUserRepositories(@PathVariable String username) throws ExecutionException, InterruptedException {
         List<Response> userRepos = githubService.getUserRepos(username);
         return ResponseEntity.ok(userRepos);
     }
