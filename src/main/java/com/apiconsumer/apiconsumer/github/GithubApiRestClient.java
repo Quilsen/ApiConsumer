bb4aafa1132a804d83b3c784429cd3bf21e34e11
Quilsen <70715580+Quilsen@users.noreply.github.com>
2024-02-20 11:26:47 +0100

    writen GithubApiRestClient

--- src/main/java/com/apiconsumer/apiconsumer/github/GithubApiRestClient.java
+++ src/main/java/com/apiconsumer/apiconsumer/github/GithubApiRestClient.java
@@ -1,4 +1,29 @@
 package com.apiconsumer.apiconsumer.github;
 
-public class GithubApiRestClient {
+import com.apiconsumer.apiconsumer.github.branch.Branch;
+import com.apiconsumer.apiconsumer.github.repo.Repo;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+public class GithubApiRestClient implements GithubClient{
+    private RestClient restClient = RestClient.create("https://api.github.com");
+
+    public List<Repo> getReposByUsername(String username){
+        return restClient.get()
+                .uri("/users/{username}/repos", username)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
+
+    public List<Branch> getBranchNameAndSha(String username, String reponame){
+        return restClient.get()
+                .uri("/repos/{username}/{reponame}/branches", username, reponame)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
+
 }
